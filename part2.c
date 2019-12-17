e39bf6539d1b6f75860da1ec5c26efed399510ab
root <root@localhost.localdomain>
2019-12-17 15:16:31 -0500

    Adjust to Linux

--- part2.c
+++ part2.c
@@ -1,29 +1,15 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX_ROWS 250
+#define MAX_PER_ROW 256
 
-
-char s[256];
-char word[30];
-char wordToSearch[30];
-char letter[2];
-
-int getLine1(char s[]){
-int i=0,count=0;
-while(s[i]!=0){
-    if(s[i]=='\n'){
-       return count; 
-}
-i++;
-count++;
-}
-return count;
-}
-
-//just to print in line
+//Print the string
 void println(char* str1){
     int i=0;
+    if(str1[i]=='\n'){
+        return;
+    }
    while (str1[i]!=0)
    {
        printf("%c",str1[i]);
@@ -57,58 +43,12 @@ int substring(char* str1, char* str2){
     return 0;
 
 }
-char* Buildword(char w[],int choice){
-    int i=0;
-    if(choice==0){
-    while(w[i]!=0){
-        if(w[i]=='\n' || w[i]== ' ' || w[i]=='\t'){
-            break;
-        }
-        word[i]=w[i];
-        i++;
-    }
-   
-   
-     word[i]=0;
-     return word;
-    }
-    //For WordToSearch
-    if(choice==1){
-        while(w[i]!=0){
-        if(w[i]=='\n' || w[i]== ' ' || w[i]=='\t'){
-            break;
-        }
-        wordToSearch[i]=w[i];
-        i++;
-    }
-   
-   
-     wordToSearch[i]=0;
-     return wordToSearch;
-    }
 
-    //letter builder 
-     if(choice==2){
-        while(w[i]!=0){
-        if(w[i]=='\n' || w[i]== ' ' || w[i]=='\t'){
-            break;
-        }
-        letter[i]=w[i];
-        i++;
-    }
-   
-   
-     letter[i]=0;
-     return letter;
-    }
- }
-
-       //     caats      cat     2
 int similar (char* s, char* t, int n){
     if (strlen(t)>strlen(s)){
         return 0;
     }
-    if (strlen(t)==strlen(s) && n==0) {
+    if (strlen(t)==strlen(s) ) {
         return substring(s,t);
     }
     int i=0, j=0,counter=0;
@@ -132,113 +72,64 @@ int similar (char* s, char* t, int n){
     return 0;
 }
 
-int countLetters(char s[]){
-    int count=0,i=0;
-    while(s[i]!=0){
-          if(s[i]=='\n' || s[i]== ' ' || s[i]=='\t'){
-              return count;
-          }
-          count++;
-          i++;
-    }
-    return count;
-}
 
 void print_similar_words(char* str,char* s){
     int i=0,num=0;
-    char* ptr=NULL;
-    while (s[i]!=0)
-    {
-       num=countLetters(s+i);
-       ptr=Buildword(s+i,0);
-       if(strlen(ptr)==strlen(str)){
-           similar(ptr,str,0);
-       }
-       else{
-           similar(ptr,str,1);
-       }
-       
-       ptr=NULL;
-       i+=num+1;
-    } 
+    similar(str,s,1);
 }
 
-char* BulidNewString(char* ptr){
-    int i=0;
-    while(*(ptr+i)!=0){
-        if(*(ptr+i)=='\n'){
-            break;
-        }
-        s[i]=*(ptr+i);
-        i++;
-    }
-    s[i]=0;
-    return s;
-
-}
-
-
-void print_lines(char* s,char *s_ptr){
-    // char* ptr=NULL;
-    int i=0,num=0;
-    // while(s[i]!=0){
-        // num = getLine1(s+i);
-        // ptr=BulidNewString(s+i);
-        substring(s,s_ptr);
-        
-              
-        
-    // }
-
-
-
-}
-
-
-
-int main(int argc,char* argv[]) {
-    int num=0;
+int main() {
+    char letter;
     int count=0;
-    char* letter=NULL;
-    char* wordToSearch=NULL;
-    char* Line=NULL;
-    char* var=NULL;
-    char* lineToCompare=NULL;
-     FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    fp = fopen(argv[1], "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
+    char buffer;
+    char Line[MAX_PER_ROW];
+    char check [30];
+    int i=0,j=0;
+    scanf("%c",&letter);
+    while(letter!=' '){
+        check[i]=letter;
+        i++;
+        scanf("%c",&letter);
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-        var = line;
-        if(count==0){
-             num = countLetters(line);
-             wordToSearch = Buildword(line,1);//Build word to search 
-             letter = Buildword(line+num+1,2);//choose which action to do 
-        }
-        if(count>1){
-            if(*(letter) == 'a'){
-                lineToCompare = BulidNewString(line);
-                print_lines(lineToCompare,wordToSearch);
+    }
+    check[i]='\0';
+    scanf("%c\n",&letter);
+
+
+    if(letter == 'a'){
+        for(i=0;i<MAX_ROWS;i++){
+            scanf("%c",&buffer);
+            count++;
+            while(buffer!='\n' && count<MAX_PER_ROW ){
+                Line[j++]=buffer;
+                scanf("%c",&buffer);
                 
             }
+            Line[j]=0;
+            j=0;
+            substring(Line,check);
+            count=0;            
+            
 
-            if(*(letter) == 'b'){
-                print_similar_words(wordToSearch,line);
-           }
-           
         }
-        count++;
-
-        
-
-        
     }
+     if(letter == 'b'){
+        for(i=0;i<MAX_ROWS;i++){
+            scanf("%c",&buffer);
+            count++;
+            while(buffer!='\n' && count<MAX_PER_ROW && buffer!=' '){
+                Line[j++]=buffer;
+                scanf("%c",&buffer);
+                
+            }
+            Line[j]=0;
+            j=0;
+            print_similar_words(Line,check);
+            count=0;            
+            
 
-    fclose(fp);
+        }
+    }
     
     return 0;
 }
