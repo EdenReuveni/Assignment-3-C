d219644fa93f203cd3c719448482da165bb6142b
galbar07 <53149608+galbar07@users.noreply.github.com>
2019-12-17 19:24:37 -0500

    tset

--- part2.c
+++ part2.c
@@ -3,42 +3,81 @@
 #include <stdlib.h>
 #define MAX_ROWS 250
 #define MAX_PER_ROW 256
+#define WORD_LENGTH 30
 
 
-
+ 
 
 
 //Print the string
  void println(char* str1){
      int i=0;
-    if(str1[i]=='\n'){
+    if(str1[i]=='\n'||str1[i]==' '){
         return;
     }
     while (str1[i]!=0)
     {
         printf("%c",str1[i]);
-int substring(char* str1, char* str2){
-     return 0;
+        i++;
  
+     }  
+     printf("\n");
  }
 
+
+ int substring(char* str1, char* str2){
+    int i=0,j=0;
+    while(str1[i] != 0){
+        j=0;                  //i=0    j=0
+        if(str1[i]==str2[j]){ //chis   cat  
+            while(str1[i]!=0 && str2[j]!=0){
+                 j++;
+                 i++;
+                if(str1[i]!=str2[j]){
+                   break;
+                }
+                
+            }
+            if(str2[j]==0){
+                println(str1);
+                return 1;
+            }
+        }
+        i++;
+    }
+    return 0;
+
+}
+
       //     caats      cat     2
- int similar (char* s, char* t, int n){
-     if (strlen(t)>strlen(s)){
-         return 0;
-     }
-+    if (strlen(t)==strlen(s) ) {
-         return substring(s,t);
-     }
-     int i=0, j=0,counter=0;
-     int similar (char* s, char* t, int n){
-     return 0;
- }
+int similar(char* s, char* t, int n){
+    if (strlen(t)>strlen(s)){
+        return 0;
+    }
+    if (strlen(t)==strlen(s)) {
+        return substring(s,t);
+    }
+    int i=0, j=0,counter=0;
+    while (s[i]!=0 && t[j]!=0){
+        if (s[i]==t[j]){
+        i++;
+        j++;
+        counter++;
+        }
+    
+        if (s[i]!=t[j])
+        {
+            i++;
+        }
+    }
+    int diff=strlen(s)-counter;
+    if (diff==n){
+        println(s);
+        return 1;
+    }
+    return 0;
+}
   
- void print_similar_words(char* str,char* s){
-     int i=0,num=0;
-     similar(str,s,1);
- }
  
 
 int main() {
@@ -46,7 +85,8 @@ int main() {
     int count=0;
     char buffer;
     char Line[MAX_PER_ROW];
-    char check [30];
+    char check[WORD_LENGTH];
+    char similar1[WORD_LENGTH];
     int i=0,j=0;
     scanf("%c",&letter);
     while(letter!=' '){
@@ -58,53 +98,42 @@ int main() {
     check[i]='\0';
     scanf("%c\n",&letter);
  
- 
-
+    scanf("%c",&buffer);
    if(letter == 'a'){
         for(i=0;i<MAX_ROWS;i++){
-            scanf("%c",&buffer);
             count++;
             while(buffer!='\n' && count<MAX_PER_ROW ){
                 Line[j++]=buffer;
                 scanf("%c",&buffer);
-                
+                count++;
             }
             Line[j]=0;
+            scanf("%c\n",&buffer);  
             j=0;
             substring(Line,check);
             count=0;            
             
  
    }
+   }
+
+   //a test [this]
      if(letter == 'b'){
-        for(i=0;i<MAX_ROWS;i++){
-            scanf("%c",&buffer);
-            count++;
-            while(buffer!='\n' && count<MAX_PER_ROW && buffer!=' '){
-                Line[j++]=buffer;
+        for(i=0;i<MAX_ROWS ;i++){
+            while(buffer!='\n' && j<30 && buffer!=0 && buffer!='\t' && buffer!=' '){
+                similar1[j++]=buffer;
                 scanf("%c",&buffer);
-                 
-             }
-            Line[j]=0;
-            j=0;
-            print_similar_words(Line,check);
-            count=0;            
-            
- 
-            if(*(letter) == 'b'){
-                print_similar_words(wordToSearch,line);
-           }
-           
-         }
-        count++;
-
-        
-
-        
-     }
+            }
+              similar1[j]=0;
+              similar(similar1,check,1);
+              j=0;
+              scanf("%c",&buffer);
+        }
 
     
      
      return 0;
  }
+}
+
 
